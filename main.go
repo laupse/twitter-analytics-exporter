d2149fa6a89aa2c463f7835ecdd7579b03fe67b3
laupse <lgrondin@ingeniance.fr>
2022-08-12 15:47:45 +0200

    Add refresh-interval flags

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"strings"
+	"time"
 
 	"github.com/laupse/twitter-analytics-exporter/adapter/http"
 	"github.com/laupse/twitter-analytics-exporter/adapter/repository"
 	"github.com/laupse/twitter-analytics-exporter/application/services"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
@@ -16,6 +18,7 @@ func main() {
 	pflag.String("user-access-token", "", "user-specific token credentials used to authenticate OAuth 1.0a")
 	pflag.String("user-secret-token", "", "user-specific secret credentials used to authenticate OAuth 1.0a")
 	pflag.String("user-id", "", "user-specific id from where to retrieve timeline")
+	pflag.String("timeline-refresh-interval", "30s", "how often we refresh tweets analytics (30s, 1m...)")
 	pflag.Parse()
 	viper.BindPFlags(pflag.CommandLine)
 
@@ -23,10 +26,16 @@ func main() {
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	viper.AutomaticEnv()
 
+	refreshInterval, err := time.ParseDuration(viper.GetString("timeline-refresh-interval"))
+	if err != nil {
+		log.Errorf("%s", err)
+		return
+	}
+
 	twitterRepository := repository.NewTwitterRepository(viper.GetString("consumer-key"), viper.GetString("consumer-secret"), viper.GetString("user-access-token"), viper.GetString("user-secret-token"))
 	metricsService := services.NewMetricsService(twitterRepository)
 
-	go metricsService.Collect(viper.GetString("user-id"))
+	go metricsService.Collect(viper.GetString("user-id"), refreshInterval)
 
 	ginHandler := http.NewGinHandler()
 	ginHandler.SetupRoutes()
